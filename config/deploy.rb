553caa4c2b97e0dd721fc7885828161b5acd5831
Raul Bernardeli <raul@raulbcs.com.br>
2017-07-24 16:34:28 -0300

    Rework QEdu-hub capistrano
    * Remove unused infos, tasks, etc
    * Created a new deploy structure from `cap init` as a starting point

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,62 @@
 # config valid only for current version of Capistrano
 lock "3.8.2"
 
-# config valid only for current version of Capistrano
-set :stages, ["prod","prod-php5"]
-set :symfony_env,  "prod"
-set :branch, "master"
-set :application, ask("set the correct app:")
-set :repo_url, "git@github.com:Talits/qedu-hub.git"
+set :application, "qedu-hub"
+set :repo_url, "git@github.com:QEdu/qedu-hub.git"
+
+# Default branch is :master
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/var/www/qedu-hub"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
 set :pty, true
-set :deploy_via, :remote_cache
-set :http_host, "www.qedu.org.br"
-set :symfony_console_path, "bin/console"
-set :permission_method, :acl
-set :permission_method, true
-set :file_permissions_paths, [fetch(:log_path), fetch(:cache_path), "var", "web/uploads"]
-set :file_permissions_users, ["ubuntu"]
+set :tmp_dir, '/tmp/deployqeduhub'
 
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
 
-after "deploy:log_revision", "dep:node"
-after "dep:node", "deploy:set_permissions:acl"
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+set :local_user, -> { `git config user.name`.chomp }
+set :use_sudo, true
+set :symfony_console_path, 'bin/console'
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+after 'deploy:starting', 'composer:install_executable'
+after 'deploy:updated', 'notify:slack'
+
+namespace :phpfpm do
+    desc "Restarting PHP-FPM"
+    task :restart do
+        run "sudo service php7-fpm restart"
+    end
+end
+
+namespace :notify do
+  desc "Notify Slack of a deployment"
+  task :slack do
+    user = `git config user.name`.strip
+    tag = `git describe --abbrev=0 --tags`.strip
+
+    slack_command = "curl -s -X POST \"https://hooks.slack.com/services/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\" -H \"Content-Type: application/json\" -d '{\"username\": \"#{user}\",\"text\": \":qedu: *QEdu-hub* at `staging` - #{user} deployed branch `master` (#{tag}). :rocket:\",\"icon_url\": \"https://avatars2.githubusercontent.com/u/58257?v=3&s=40\"}'"
+
+    puts "--> Notifying Slack"
+    # run slack_command
+  end
+end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,25 +1,57 @@
-
-server "ec2-54-233-112-115.sa-east-1.compute.amazonaws.com", user: "ec2-user", roles: %w{php7}
-
-
-
-#variaveis
-set :composer_install_flags, '--no-dev --no-interaction --quiet --optimize-autoloader'
-set :branch,         "branch-stage"
-set :deploy_to,      "/var/www/html/#{fetch :application}"
-set :url,            "http://qa.qedu.org.br/"
-set :cache_prefix,   "qedu-stage"
-set :metrics_prefix, "qedu-stage"
-set :server_env,     10
-set :http_host,      "qa.qedu.org.br"
-set :curl_options,   "-umeritts:iwantitall"
-set :prismic_ref,    "Depoimentos"
-set :cookie_domain,  "qa.qedu.org.br"
-set :composer_install_flags, '--no-dev --no-interaction --optimize-autoloader'
+server "52.67.121.9", user: "www-data", roles: %w{app db web}
+# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+# server "db.example.com", user: "deploy", roles: %w{db}
 
 
-after "deploy:log_revision", "dep:node"
-after "dep:node", "shared:googlewmt"
-after "shared:googlewmt", "dep:permissao"
-after "dep:permissao", "dep:include"
-after "dep:include","restart:nginx"
+
+# role-based syntax
+# ==================
+
+# Defines a role with one or multiple servers. The primary server in each
+# group is considered to be the first unless any hosts have the primary
+# property set. Specify the username and a domain or IP for the server.
+# Don't use `:all`, it's a meta role.
+
+# role :app, %w{deploy@example.com}, my_property: :my_value
+# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
+# role :db,  %w{deploy@example.com}
+
+
+
+# Configuration
+# =============
+# You can set any configuration variable like in config/deploy.rb
+# These variables are then only loaded and set in this stage.
+# For available Capistrano configuration variables see the documentation page.
+# http://capistranorb.com/documentation/getting-started/configuration/
+# Feel free to add new variables to customise your setup.
+
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+set :ssh_options, {
+  user: "www-data",
+  keys: %w(~/.ssh/qedu-php7.pem),
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
+#
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server "example.com",
+#   user: "user_name",
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: "user_name", # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+#   }
