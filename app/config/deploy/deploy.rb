3eb7e64e2d9f43b9573d2df52d8403438f45c8b3
Marcus Sá <marcusesa@gmail.com>
2017-07-26 16:46:02 -0300

    Remove custom variables in Capistrano scripts
    The values used are default values.

--- app/config/deploy/deploy.rb
+++ app/config/deploy/deploy.rb
@@ -8,7 +8,6 @@ set :repo_url, "https://github.com/QEdu/qedu-hub.git"
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/qedu-hub"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -34,7 +33,6 @@ set :format_options, log_file: "var/logs/capistrano.log"
 # Default value for local_user is ENV['USER']
 set :local_user, -> { `git config user.name`.chomp }
 set :use_sudo, true
-set :symfony_console_path, 'bin/console'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
