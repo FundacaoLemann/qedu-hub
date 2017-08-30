7d6d6f28a1a55e45cc699f0feb5b29050adbcaba
Marcus Sá <marcusesa@gmail.com>
2017-08-30 18:22:24 -0300

    Update session handler and load current user
    Update session handler from native to redis, and load the current user
    based on UsernamePasswordToken.
    
    Main Changes:
    
    - Install and register SncRedisBundle and Predis.
    - Update session handler from native to redis.
    - Configure user encoder.
    - Configure database user loader.
    - Add user entity.
    - Add user repository.

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User implements UserInterface, \Serializable
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="email", type="string", length=50, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $surname;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function serialize()
+    {
+        return serialize([
+            $this->id,
+            $this->email,
+            $this->password,
+        ]);
+    }
+
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->email,
+            $this->password,
+            ) = unserialize($serialized);
+    }
+}
