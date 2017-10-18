596cee9f1f8c3ed6f23b526d4fa84e4af3f721fe
Marcus Sá <marcusesa@gmail.com>
2017-10-18 16:02:54 -0200

    Process Learning data in LearningController

--- src/AppBundle/Controller/LearningController.php
+++ src/AppBundle/Controller/LearningController.php
@@ -2,16 +2,35 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Learning\LearningService;
+use AppBundle\Learning\ProvaBrasilService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class LearningController extends Controller
 {
+    private $provaBrasilService;
+    private $learningService;
+
+    public function __construct(
+        ProvaBrasilService $provaBrasilService,
+        LearningService $learningService
+    ) {
+        $this->provaBrasilService = $provaBrasilService;
+        $this->learningService = $learningService;
+    }
+
     /**
      * @Route("/brasil/aprendizado-new", name="learning_brazil")
      */
     public function brazilAction()
     {
-        return $this->render('learning/brazil.html.twig');
+        $provaBrasilEdition = $this->provaBrasilService->getLastEdition();
+        $brazilLearning = $this->learningService->getBrazilLearningByEdition($provaBrasilEdition);
+
+        return $this->render('learning/brazil.html.twig', [
+            'provaBrasilEdition' => $provaBrasilEdition,
+            'brazilLearning' => $brazilLearning,
+        ]);
     }
 }
