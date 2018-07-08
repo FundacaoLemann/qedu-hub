1cb5972eefc45a86633610a74737b1f76dd380d6
Marcus Sá <marcusesa@gmail.com>
2018-07-08 18:34:18 -0300

    Add waterSupplierFilter in CensusDataFilter and tests

--- src/AppBundle/Util/Filter/CensusDataFilter.php
+++ src/AppBundle/Util/Filter/CensusDataFilter.php
@@ -100,6 +100,23 @@ class CensusDataFilter extends AbstractExtension
         }
     }
 
+    public function waterSupplierFilter($censusServices)
+    {
+        if ($censusServices->hasWaterPublicNetwork()) {
+            return 'Rede pública';
+        } elseif ($censusServices->hasArtesianWellWater()) {
+            return 'Poço artesiano';
+        } elseif ($censusServices->hasWaterReservoir()) {
+            return 'Cacimba';
+        } elseif ($censusServices->hasWaterRiver()) {
+            return 'Rio';
+        } elseif ($censusServices->hasNotWater()) {
+            return 'Inexistente';
+        }
+
+        return 'Não informado';
+    }
+
     public function getFilters()
     {
         return [
@@ -109,6 +126,7 @@ class CensusDataFilter extends AbstractExtension
             new TwigFilter('addressFilter', [$this, 'addressFilter']),
             new TwigFilter('optionalNumberTranslationFilter', [$this, 'optionalNumberTranslationFilter']),
             new TwigFilter('convertNumberToYesNoFilter', [$this, 'convertNumberToYesNoFilter']),
+            new TwigFilter('waterSupplierFilter', [$this, 'waterSupplierFilter']),
         ];
     }
 }
--- tests/unit/AppBundle/Util/Filter/CensusDataFilterTest.php
+++ tests/unit/AppBundle/Util/Filter/CensusDataFilterTest.php
@@ -217,18 +217,73 @@ class CensusDataFilterTest extends TestCase
                 $outputExpected = "<span style='color: red'>Não</span>",
             ],
             [
-                $$number = 1,
+                $number = 1,
                 $outputExpected = "<span style='color: darkgreen'>Sim</span>",
             ],
             [
-                $$number = null,
+                $number = null,
                 $outputExpected = "<span style='color: #666'>Não informado</span>",
             ],
             [
-                $$number = '',
+                $number = '',
                 $outputExpected = "<span style='color: #666'>Não informado</span>",
             ],
         ];
+    }
+
+    /**
+     * @dataProvider waterSupplierFilterDataProvider
+     */
+    public function testWaterSupplierFilter($servicesMock, $outputExpected)
+    {
+        $censusDataFilter = new CensusDataFilter();
+        $output = $censusDataFilter->waterSupplierFilter($servicesMock);
+
+        $this->assertEquals($outputExpected, $output);
+    }
+
+    public function waterSupplierFilterDataProvider()
+    {
+        return [
+            [
+                $method = $this->methodWillReturnTrue('hasWaterPublicNetwork'),
+                $outputExpected = 'Rede pública',
+            ],
+            [
+                $method = $this->methodWillReturnTrue('hasArtesianWellWater'),
+                $outputExpected = 'Poço artesiano',
+            ],
+            [
+                $method = $this->methodWillReturnTrue('hasWaterReservoir'),
+                $outputExpected = 'Cacimba',
+            ],
+            [
+                $method = $this->methodWillReturnTrue('hasWaterRiver'),
+                $outputExpected = 'Rio',
+            ],
+            [
+                $method = $this->methodWillReturnTrue('hasNotWater'),
+                $outputExpected = 'Inexistente',
+            ],
+            [
+                $method = $this->createCensusServicesMock(),
+                $outputExpected = 'Não informado',
+            ],
+        ];
+    }
+
+    private function methodWillReturnTrue($methodName)
+    {
+        $services = $this->createCensusServicesMock();
+        $services->method($methodName)
+            ->willReturn(true);
+
+        return $services;
+    }
+
+    private function createCensusServicesMock()
+    {
+        return $this->createMock('AppBundle\Entity\Census\Group\Services');
     }
 
     public function testGetFiltersShouldReturnAllFiltersRegistered()
@@ -236,6 +291,6 @@ class CensusDataFilterTest extends TestCase
         $censusDataFilter = new CensusDataFilter();
         $filters = $censusDataFilter->getFilters();
 
-        $this->assertCount(6, $filters);
+        $this->assertCount(7, $filters);
     }
 }
