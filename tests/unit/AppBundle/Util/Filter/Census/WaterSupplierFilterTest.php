97bfb39eb638c83e9f4f97dfd4c7bea12a194138
Marcus Sá <marcusesa@gmail.com>
2018-07-08 23:58:39 -0300

    Add WaterSupplierFilter
    Move WaterSupplierFilter from CensusDataFilter to specific filter.

---
+++ tests/unit/AppBundle/Util/Filter/Census/WaterSupplierFilterTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Unit\AppBundle\Util\Filter\Census;
+
+use AppBundle\Util\Filter\Census\WaterSupplierFilter;
+use PHPUnit\Framework\TestCase;
+
+class WaterSupplierFilterTest extends TestCase
+{
+	public function testClassShouldBeInstanceOfAbstractExtension()
+    {
+        $waterSupplierFilter = new WaterSupplierFilter();
+
+        $this->assertInstanceOf('Twig\Extension\AbstractExtension', $waterSupplierFilter);
+    }
+
+	/**
+     * @dataProvider waterSupplierFilterDataProvider
+     */
+    public function testWaterSupplierFilter($servicesMock, $outputExpected)
+    {
+        $waterSupplierFilter = new WaterSupplierFilter();
+        $output = $waterSupplierFilter->waterSupplierFilter($servicesMock);
+
+        $this->assertEquals($outputExpected, $output);
+    }
+
+    public function waterSupplierFilterDataProvider()
+    {
+        return [
+            [
+                $method = $this->methodWillReturnTrue('hasWaterPublicNetwork'),
+                $outputExpected = 'Rede pública',
+            ],
+            [
+                $method = $this->methodWillReturnTrue('hasArtesianWellWater'),
+                $outputExpected = 'Poço artesiano',
+            ],
+            [
+                $method = $this->methodWillReturnTrue('hasWaterReservoir'),
+                $outputExpected = 'Cacimba',
+            ],
+            [
+                $method = $this->methodWillReturnTrue('hasWaterRiver'),
+                $outputExpected = 'Rio',
+            ],
+            [
+                $method = $this->methodWillReturnTrue('hasNotWater'),
+                $outputExpected = 'Inexistente',
+            ],
+            [
+                $method = $this->createCensusServicesMock(),
+                $outputExpected = 'Não informado',
+            ],
+        ];
+    }
+
+    private function methodWillReturnTrue($methodName)
+    {
+        $services = $this->createCensusServicesMock();
+        $services->method($methodName)
+            ->willReturn(true);
+
+        return $services;
+    }
+
+    private function createCensusServicesMock()
+    {
+        return $this->createMock('AppBundle\Entity\Census\Group\Services');
+    }
+
+    public function testGetFiltersShouldReturnFilterRegistered()
+    {
+        $waterSupplierFilter = new WaterSupplierFilter();
+        $filters = $waterSupplierFilter->getFilters();
+
+        $this->assertCount(1, $filters);
+    }
+}
